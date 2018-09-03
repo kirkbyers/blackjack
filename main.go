eabb2f939493dee7787438806ec06be5f0e87fe5
kirkbyers <kirklbyers@gmail.com>
2018-09-03 15:49:42 -0500

    Complete basic Blackjack game

--- main.go
+++ main.go
@@ -54,47 +54,50 @@ func min(a, b int) int {
 	return b
 }
 
-func main() {
-	cards := deck.New(deck.Deck(3), deck.Shuffle)
-	var (
-		player, dealer Hand
-		card           deck.Card
-	)
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
+	var card deck.Card
 	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
 	}
+	return ret
+}
 
-	var input string
-	for input != "s" {
-		fmt.Println("Dealer shows:", dealer.DealerString())
-		fmt.Println("Player hand:", player)
-		fmt.Println("(h)it or (s)tand?")
-		fmt.Scanf("%s\n", &input)
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		case "s":
-		case "":
-		case "\n":
-		default:
-			fmt.Println("***INVALID INPUT***")
-			time.Sleep(1 * time.Second)
-		}
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		return Stand(ret)
 	}
+	return ret
+}
 
-	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
 
-	pScore, dScore := player.Score(), dealer.Score()
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
 	fmt.Println("===FINAL HANDS===")
-	fmt.Println("Dealer hand:", dealer, "\nDealer score:", dScore)
-	fmt.Println("Player hand:", player, "\nPlayer score:", pScore)
+	fmt.Println("Dealer hand:", ret.Dealer, "\nDealer score:", dScore)
+	fmt.Println("Player hand:", ret.Player, "\nPlayer score:", pScore)
 	switch {
 	case pScore > 21:
 		fmt.Println("You busted")
@@ -107,8 +110,94 @@ func main() {
 	case pScore == dScore:
 		fmt.Println("Draw")
 	}
+	fmt.Println()
+	ret.Player = nil
+	ret.Dealer = nil
+	ret.State = StatePlayerTurn
+	return ret
+}
+
+func main() {
+	var gs GameState
+	gs = Shuffle(gs)
+
+GameLoop:
+	for gs.State >= 0 {
+		gs = Deal(gs)
+
+		var input string
+		for gs.State == StatePlayerTurn {
+			fmt.Println("Dealer shows:", gs.Dealer.DealerString())
+			fmt.Println("Player hand:", gs.Player)
+			fmt.Println("(h)it, (s)tand, or (e)xit")
+			fmt.Scanf("%s\n", &input)
+			switch input {
+			case "h":
+				gs = Hit(gs)
+			case "s":
+				gs = Stand(gs)
+			case "e":
+				break GameLoop
+			case "":
+			case "\n":
+			default:
+				fmt.Println("***INVALID INPUT***", input)
+				time.Sleep(1 * time.Second)
+			}
+		}
+
+		for gs.State == StateDealerTurn {
+			if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+				gs = Hit(gs)
+			} else {
+				gs = Stand(gs)
+			}
+		}
+
+		gs = EndHand(gs)
+	}
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
+}
+
+type State int8
+
+const (
+	StatePlayerTurn State = iota
+	StateDealerTurn
+	StateHandOver
+)
+
+// GameState is the current state of the game
+type GameState struct {
+	Deck   []deck.Card
+	State  State
+	Player Hand
+	Dealer Hand
+}
+
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case StatePlayerTurn:
+		return &gs.Player
+	case StateDealerTurn:
+		return &gs.Dealer
+	default:
+		panic("In unknown game state")
+	}
+}
+
+func clone(gs GameState) GameState {
+	ret := GameState{
+		Deck:   make([]deck.Card, len(gs.Deck)),
+		State:  gs.State,
+		Player: make(Hand, len(gs.Player)),
+		Dealer: make(Hand, len(gs.Dealer)),
+	}
+	copy(ret.Deck, gs.Deck)
+	copy(ret.Player, gs.Player)
+	copy(ret.Dealer, gs.Dealer)
+	return ret
 }
