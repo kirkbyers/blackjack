01f32684ad38c9379fbff48a070b7527413693f1
kirkbyers <kirklbyers@gmail.com>
2018-09-03 14:12:05 -0500

    Init basic game.

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/kirkbyers/deck"
+)
+
+// Hand represents a player's hand
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+// DealerString prints the hand, showing only the first card and hidding the rest
+func (h Hand) DealerString() string {
+	return h[0].String() + ", *HIDDEN*"
+}
+
+func main() {
+	cards := deck.New(deck.Deck(3), deck.Shuffle)
+	var (
+		player, dealer Hand
+		card           deck.Card
+	)
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
+	}
+
+	var input string
+	for input != "s" {
+		fmt.Println("Dealer shows:", dealer.DealerString())
+		fmt.Println("Player hand:", player)
+		fmt.Println("(h)it or (s)tand?")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		case "s":
+		case "":
+		case "\n":
+		default:
+			fmt.Println("***INVALID INPUT***")
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	fmt.Println("===FINAL HANDS===")
+	fmt.Println("Dealer shows:", dealer)
+	fmt.Println("Player hand:", player)
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
+}
